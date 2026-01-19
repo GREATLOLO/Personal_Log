2a0a51564a7a55954e3cf96ee9ff90a3bd573071
Keqing Li <kel062@ucsd.edu>
2026-01-19 15:05:42 -0800

    key_name_fix

--- lib/agent.ts
+++ lib/agent.ts
@@ -1,7 +1,7 @@
 import { LlmAgent } from "@google/adk";
-import { GoogleGenerativeAI } from "@google/genai";
+import { GoogleGenAI } from "@google/genai";
 
-const genai = new GoogleGenerativeAI(process.env.GOOGLE_GENAI_API_KEY!);
+const genai = new GoogleGenAI({ apiKey: process.env.GOOGLE_GENAI_API_KEY! });
 
 /**
  * Mirrors:
